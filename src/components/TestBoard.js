ac62daf995b0ee2a8426ca23eea478682cdf703e
icedaman <36274654+icedaman@users.noreply.github.com>
2020-07-03 20:11:48 +0100

    Update TestBoard.js

--- src/components/TestBoard.js
+++ src/components/TestBoard.js
@@ -2,65 +2,84 @@ import React, {useState} from 'react';
 import './index.css';
 
 export const TestBoard = () => {
+  const [inputSize, setInputSize] = useState(null);
+  const [board, setBoard] = useState(Array.from({length: inputSize},()=> Array.from({length: inputSize}, () => null)));
+  const [xIsNext, setXIsNext] = useState(true);
 
-const [inputSize, setInputSize] = useState(null);
+  const handleSquareUpdate = (row, column) => {
+    let updatedBoard = [...board];
 
-const [board, setBoard] = useState([...Array().fill(' ')]);
-const [square, setSquare] = useState('');
+    if (updatedBoard[row][column] == null) {
+      
+      if (xIsNext){
+        updatedBoard[row][column] = 'X';
+      } else {
+        updatedBoard[row][column] = 'O';
+      }
+      setXIsNext(!xIsNext);
+    }
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  if (Number(inputSize)) {
-    setBoard(() => [...Array(Number(inputSize)).fill(' ')]);
-  }else {
-    return;
+    calculateWinner(updatedBoard);
+    setBoard(updatedBoard);
+
+    // console.log(board);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (Number(inputSize)) {
+      setBoard(Array.from({length: inputSize},()=> Array.from({length: inputSize}, () => null)));
+      setInputSize(null);
+    }else {
+      return;
+    }  
   }  
-}
 
-const handleClick = (square) => {
-  setSquare(() => [...square='x'])
-}
+  const calculateWinner = (updatedBoard) => {
+    updatedBoard.map((rowElement, rowIndex) => {
+      if( !!rowElement.reduce(function(a, b){ return (a === b) ? a : NaN; })  ) {
+        alert('Congratz you Won!');           
+      }
+      rowElement.map((colElement, colIndex) => {
+        // console.log('row index '+ rowIndex+ " col index: "+colIndex+ " value: "+colElement);
 
- const drawBoard = () => {
-   
-    return (
-            <div className="mainContainer">
-              {board.map((row, indexRow) =>(
-                <>
-                {board[indexRow]=[]}
-                <div className="row" key={indexRow}>
-                  {board.map((col, indexCol) =>(
-                  board[indexRow][indexCol] = square,
-                    <>
-                    {console.log(square)}
-                    <button
-                      onClick={() => handleClick(square)} 
-                      className="testRow"
-                      key={indexCol}
-                    >
-                      {square}
-                    </button>
-                    </>
-                  ))}
-                </div>
-                </>
-              ))}
-            </div>
-    );
- }
-
+      })
+    })
+  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit} style={{textAlign:'center'}}>
-        <label>
-          Board Size:
-          <input type="text" value={inputSize} onChange={(event)=> setInputSize(event.target.value)} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      {drawBoard()}
+      {}
+       <form onSubmit={handleSubmit} className="formInputSize">
+          <label>
+            Board Size:
+            <input type="text" value={Number(inputSize)} onChange={(event)=> setInputSize(event.target.value)} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      <div className="mainContainer">
+        <table>
+          <tbody>
+            {board.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((column, columnIndex) => (
+                  <td key={columnIndex}>
+                    <button
+                      className="testRow"
+                      onClick={() => handleSquareUpdate(rowIndex, columnIndex)}
+                    >
+                      {board[rowIndex][columnIndex]}
+                    </button>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="formInputSize">
+        {"Player to act : " + (!xIsNext ? 'O': 'X')}
+      </div>
     </div>
-
   );
 };
